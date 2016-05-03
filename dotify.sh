e329752fd1cf1ea66775fe6aee0a56acc41a9564
Cooper Young <coopery@umich.edu>
2016-05-02 22:24:44 -0400

    Moved i3 stuff to .config/i3. Updated dotify to deal with .config folder

--- dotify.sh
+++ dotify.sh
@@ -1,18 +1,32 @@
 # This script configures the home directory to use shared config files
 # It creates symbolic links to all the files in this repo
 
-Ignore=('.' '..' '.git')
+ignore_home=('.' '..' '.git' '.config')
 
 # Remove non-shared config files
 # Create symbolic links to shared config files
 for filename in .*; do
-    # Ignore names in Ignore
-    if [[ ! " ${Ignore[@]} " =~ " $filename " ]]; then
+    # Ignore names in ignore_home
+    if [[ ! " ${ignore_home[@]} " =~ " $filename " ]]; then
         echo Removing: "~"/$filename
         rm -rf ~/$filename
         echo Creating: "~"/$filename "-> ~/"${PWD##*/}/$filename
         ln -s $PWD/$filename ~/$filename
     else
         echo Ignoring: $filename;
+    fi
+done;
+
+ignore_config=('.' '..')
+
+for filename in .config/*; do
+    # Ignore names in ignore_config
+    if [[ ! " ${ignore_config[@]} " =~ " $filename " ]]; then
+        echo Removing: "~/.config"/$filename
+        rm -rf ~/.config/$filename
+        echo Creating: "~/.config"/$filename "-> ~/.config/"${PWD##*/}/$filename
+        ln -s $PWD/$filename ~/.config/$filename
+    else
+        echo Ignoring: .config/$filename;
     fi
 done;
